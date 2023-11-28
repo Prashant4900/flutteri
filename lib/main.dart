7987585fb957174691143d32e41dd10e79e2d96c
Prashant Nigam <62440911+Prashant4900@users.noreply.github.com>
2023-11-28 11:21:03 +0530

    splash screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:flutteri/app/app.dart';
 
 void main() {
+  final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   setUrlStrategy(PathUrlStrategy());
+  FlutterNativeSplash.remove();
   runApp(const MyApp());
 }
